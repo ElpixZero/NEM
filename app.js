008b93ef8c32885ee3564e5ac667b3205264cda2
Dager <nasurov.eldar@mail.ru>
2019-02-18 00:57:07 +0300

    Adding and setting path for express

--- app.js
+++ app.js
@@ -2,31 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Post = require('./Models/post.js');
+const path = require('path');
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
 
-let arr = ['hello', 'world', 'ichangeyou'];
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  '/javascripts',
+  express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
+
 
 app.get('/', function (req, res) {
-  Post.find({}) // Post receives Promise and you can use .then/.catch and the same in createPost below
-  .then(post => res.render('index', { post })
-  )
-}); 
-
-app.get('/create', function (req, res) {
-  res.render('create')
-}); 
-
-app.post('/create', function (req, res) {
-  const {title, body} = req.body;
-
-  Post.create({
-    title,
-    body
-  })
-  .then( post => console.log(post));
-  res.redirect('/');
+  res.render('index');
 }); 
 
 module.exports = app;
