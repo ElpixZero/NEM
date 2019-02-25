c84714549ec87014a546e526b31c5871e05ed186
Dager <nasurov.eldar@mail.ru>
2019-02-26 01:06:13 +0300

    Move all settings of database to app.js

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 /* eslint-disable node/no-extraneous-require*/
 /* eslint-disable node/no-unpublished-require*/
+
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const staticAsset = require('static-asset'); // for reqired refreshing page
 // const Post = require('./Models/post.js');
 const path = require('path');
+const config = require('./config');
+const mongoose = require('mongoose');
 
+// database
+mongoose.Promise = global.Promise;
+mongoose.set('debug', config.IS_PRODUCTION);
+
+mongoose.connection
+  .on('error', error => console.log(error))
+  .on('close', () => console.log('Db connectd closed'))
+  .on('open', () => {
+    const info = mongoose.connection;
+    console.log(`Connected to ${info.host}:${info.port}:${info.name}`);
+  });
+
+mongoose.connect(config.MONGO_URL, { useNewUrlParser: true });
+
+
+// express
+const app = express();
+
+// sets and uses
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(staticAsset(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,9 +37,32 @@ app.use(
   express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
 );
 
-
+// routers
 app.get('/', function (req, res) {
   res.render('index');
 }); 
 
-module.exports = app;
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+//error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.render('error', {
+    message: error.message,
+    error: !config.IS_PRODUCTION ? error : {},
+    title: 'Ooops...'
+  });
+});
+
+
+app.listen(config.PORT, function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`server is connected on ${config.PORT} port`);
+});
